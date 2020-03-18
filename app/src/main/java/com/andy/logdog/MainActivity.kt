74ad267f79d7f13dcf933e6bce1880ab59f10b3f
AndyWu <andySmily@163.com>
2020-03-18 23:06:36 +0800

    [refactor]:{convert MainActivity.java ---> MainActivity.kt}

---
+++ app/src/main/java/com/andy/logdog/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.andy.logdog
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val file = File(applicationContext
+                .getExternalFilesDir(null), "logdog")
+        Logdog.path = file.absolutePath
+
+
+        // Example of a call to a native method
+        btn_read.setOnClickListener { readFile() }
+        btn_write_mmap.setOnClickListener { writeInputWithMMAP() }
+        btn_init.setOnClickListener { Logdog.getInstance().init(application) }
+    }
+
+    private fun getEditInput(): String {
+        if(et.text.isEmpty()) return ""
+        return et.text.toString()
+    }
+
+    private fun writeInputWithMMAP() {
+        Logdog.getInstance().w(getEditInput())
+        et.setText("")
+    }
+
+    private fun readFile() {
+        val read = Logdog.getInstance().readFile(Logdog.path)
+        Log.i("Main", "read from file: $read")
+        tv.text = read
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Logdog.getInstance().onExit()
+    }
+}
